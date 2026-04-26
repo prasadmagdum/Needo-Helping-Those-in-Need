e1cb33e24daeb48c2b151c98589645f2ad1c304b
prasadmagdum <prasadmagdum307@gmail.com>
2026-04-26 18:46:25 +0530

    apipath&envchangefrontend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,29 @@
-// src/api/axios.js
+// // src/api/axios.js
+// import axios from "axios";
+
+// const API = axios.create({
+//   baseURL: "http://localhost:5000/api", // update if your backend URL differs
+// });
+
+// API.interceptors.request.use(
+//   (config) => {
+//     const stored = localStorage.getItem("auth");
+//     if (stored) {
+//       const { token } = JSON.parse(stored);
+//       if (token) config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default API;
+
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // update if your backend URL differs
+  baseURL: import.meta.env.VITE_API_URL + "/api",
 });
 
 API.interceptors.request.use(
